15196af1143c3c8a093e5a80903243abeecca257
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-12-04 22:59:07 +0100

    Revert "Remove everything except the / route"
    This reverts commit 25b3c126c3392b190f00f7329ee2a2be149a1820.

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const router = express.Router();
+var indexRouter = require('./routes/index');
 
 var app = express();
 
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-router.get('/', function(req, res, next) {
-  res.json({message: 'alive', test: process.env.TEST});
-});
-  
+app.use('/', indexRouter);
+
 module.exports = app;
