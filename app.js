6e0915466d163da29d0685498f08e74f0d111e4d
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-12-04 23:06:56 +0100

    Inline the indexRouter module

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 
-var indexRouter = require('./routes/index');
-
 var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
+var router = express.Router();
+router.get('/', function(req, res, next) {
+  res.json({message: 'alive', test: process.env.TEST});
+});
+app.use('/', router);
 
 module.exports = app;
