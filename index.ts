61a695c9334c3d7df2fef774fb2343a78c9ce4a7
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-12-05 18:09:00 +0100

    Convert to typescript

--- index.ts
+++ index.ts
@@ -2,19 +2,17 @@
 import express, { json, urlencoded, Router } from 'express';
 import { createServer } from 'http';
 
-var app = express();
+const app = express();
 
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
 var router = Router();
-router.get('/', function(req, res, next) {
+router.get('/', (req: unknown, res: any) => {
   res.json({message: 'alive', test: process.env.TEST});
 });
 app.use('/', router);
 
 const port = process.env.PORT ?? 80
-app.set('port', port);
-
 const server = createServer(app);
 server.listen(port);
